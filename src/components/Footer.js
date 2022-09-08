4fa0a7bfadd1dd95d8e5957b395d70348080fb49
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-09-08 11:32:06 -0500

    Initial

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="copyright">
+            <p>Made with love by Myself</p>
+        </div>
+    )
+}
+
+export default Footer
